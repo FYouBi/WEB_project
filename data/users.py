28fcb5f0096755c7125940fa7aac0004d0d84f94
cliss <95913122+cliss1@users.noreply.github.com>
2022-03-30 16:29:56 +0300

    Update users.py

--- data/users.py
+++ data/users.py
@@ -6,14 +6,11 @@ from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase):
-    __tablename__ = 'auth'
+    __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    # surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     phone_number = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
-    # address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    # ads = orm.relation("Ads", back_populates='user')
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    ads = orm.relation("Ads", back_populates='user')
