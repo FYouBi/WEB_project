0bad2fd0e1d3ecf94d76e95141cd770c540ffecc
cliss <95913122+cliss1@users.noreply.github.com>
2022-03-30 16:22:59 +0300

    добавлены функции проверки данных пользователя и вызов страницы

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from flask import Flask, render_template, url_for, request, redirect
 import datetime
-from data.ads import Jobs
-from forms import LoginForm, Protection, LoadPhoto, RegisterForm
+from data.ads import Ads
+from forms import LoginForm, RegisterForm
 from data import db_session
 from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def check_password(data):
+    if len(data) < 8:
+        return 'Мало символов(Нужно больше 8)'
+    elif ('%' in data) or ('@' in data) or ('#' in data) or ('$' in data) or ('!' in data) or ('^' in data) or \
+            ('&' in data) or ('*' in data) or ('=' in data) or ('+' in data) or ('-' in data) or ('`' in data) \
+            or (' ' in data):
+        return 'Недопустимые символы'
+    elif len(data) > 30:
+        return 'Слишком много символов(Нужно меньше 30)'
+    elif data.isupper():
+        return 'Нужны символы в нижнем регистре'
+    elif data.isdigit():
+        return 'Нужны буквы'
+    elif data.isalpha():
+        return 'Нужны цифры'
+    for i in data:
+        if i.isupper():
+            return True
+    return 'Нужны символы в верхнем регистре'
+
+
+def check_phone(data):
+    data = data[1::] if data[0] == '+' else data
+    if len(data) < 16:
+        for i in data:
+            if not i.isdigit():
+                return False
+        return True
+    else:
+        return False
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -16,14 +48,25 @@ def register():
     if form.validate_on_submit():
         data = form.data
         print(data)
+
+        if [user for user in session.query(User).filter(User.phone_number == data['phone_number'])]:
+            return render_template('register.html', title='Авторизация', form=form,
+                                   message='Данный номер телефона уже зарегистрирован')
+        if not check_phone(data['phone_number']):
+            return render_template('register.html', title='Авторизация', form=form,
+                                   message='Неверный формат номера')
+        if type(check_password(data['password'])) != bool:
+            return render_template('register.html', title='Авторизация', form=form,
+                                   message=f'{check_password(data["password"])}')
         new_user = User(name=data['name'],
                         phone_number=data['phone_number'],
-                        password=data['password'])
+                        password=data['password'],
+                        address=data['address'])
         session.add(new_user)
         USER = [user for user in session.query(User).filter(User.phone_number == data['phone_number'])]
         session.commit()
         return render_template('essential.html', list_of_ad=list_of_ad, user=USER)
-    return render_template('register.html', title='Авторизация', form=form)
+    return render_template('register.html', title='Авторизация', form=form, message='')
 
 
 @app.route('/log_in', methods=['GET', 'POST'])
@@ -39,11 +82,16 @@ def log_in():
                     USER = [user for user in session.query(User).filter(User.phone_number == form.data['phone_number'])]
                     return render_template('essential.html', list_of_ad=list_of_ad, user=USER)
                 return render_template('log_in.html', title='log_in', message='Неверный пароль',
-                                       form=form, list=[form.phone_number, form.password])
+                                       form=form)
         return render_template('log_in.html', title='log_in', message='Неверный номер телефона',
-                               form=form, list=[form.phone_number, form.password])
+                               form=form)
     return render_template('log_in.html', title='log_in', message='',
-                           form=form, list=[form.phone_number, form.password])
+                           form=form)
+
+
+@app.route('/create_ad')
+def create_ad():
+    return render_template('create.html')
 
 
 @app.route('/')
